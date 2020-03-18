c2232023d84706a384866b0e723ad1555c0199e4
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-03-18 23:40:57 +0100

    V2

--- holberton.h
+++ holberton.h
@@ -1,7 +1,22 @@
 #ifndef _PRINTF_
 #define _PRINTF_
-
-int _printf (const char *format, ...);
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
 int _putchar(char c);
+void _puts(char *str);
+char *_strncat(char *dest, char *src, int n);
+int _strlen(char *s);
+void write_per(char *dest,va_list arg);
+void write_char(char *src, va_list arg);
+void write_str(char *src, va_list arg);
 
+int _printf(const char *format, ...);
+typedef struct Printer
+{
+	char op;
+	void (*p)(char *str,va_list arg);
+} Printer;
 #endif /* _PRINTF_ */
