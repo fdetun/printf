6e15368924b5b27d18cd60a53bcd552f552915b1
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-03-18 23:58:38 +0100

    V22

--- _printf.c
+++ _printf.c
@@ -1,85 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <unistd.h>
 #include "holberton.h"
 
-
-/**
- * fde - fde
- * @f1: f1
- * Return: Always 0
- */
-void fde(va_list f1)
-{
-char c = (char)va_arg(f1, int);
-_putchar(c);
-}
-
-/**
- * s - s
- * @f1: f1
- * Return: Always 0
- */
-void s(va_list f1)
-{
-int j = 0;
-char *ch;
-ch = malloc(1024);
-ch = va_arg(f1, char*);
-if (!ch)
-{
-ch = "(null)";
-}
-for (j = 0; ch[j]; j++)
-{
-_putchar(ch[j]);
-}
-}
-
-/**
- * _printf - _printf
- * @format: format
- * Return: char
-*/
 int _printf(const char *format, ...)
 {
-va_list f1;
-int i = 0;
-va_start(f1, format);
+	Printer Occ[] = {
+		{'c', write_char},
+		{'s', write_str},
+		{'%', write_per},
+		{0, NULL}
+	};
+	char *stock = malloc(1024 * sizeof(char)), ch;
+	va_list arg;
+	unsigned int  i = 0, j = 0, len;
 
-while (format && format[i])
-{
-if (format[i] == '%')
-{
-i++;
-switch (format[i])
-{
-case 'c':
-fde(f1);
-break;
-case '%':
-_putchar('%');
-break;
-case '\0':
-break;
-case 's':
-s(f1);
-break;
-default:
-{
-_putchar('%');
-_putchar(format[i]);
-break;
-}
-}
-}
-else
-{
-_putchar(format[i]);
-}
-i++;
-}
-va_end(f1);
-return (i);
+	va_start(arg, format);
+	stock[0] = '\0';
+	while (format[i] && format)
+	{
+		if (format[i] != '%')
+		{
+			ch = format[i];
+			stock = _strncat(stock, &ch, 1);
+		} else
+		{
+			j = 0;
+			while (Occ[j].op)
+			{
+				if (Occ[j].op == format[i + 1])
+				{
+					Occ[j].p(stock, arg);
+					i++;
+					break;
+				}
+				j++;
+			}
+		}
+		i++;
+	}
+	va_end(arg);
+	len = _strlen(stock);
+	_puts(stock);
+	free(stock);
+	return (len);
 }
