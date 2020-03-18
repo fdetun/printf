42d0452834ae356dbb9fd1d848436594e4c60966
Foued Lamine <fouedads@gmail.com>
2020-03-18 19:35:30 +0100

    new commit

--- _printf.c
+++ _printf.c
@@ -1,26 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include <string.h>
-#include "holberton.h"
 #include <unistd.h>
+#include "holberton.h"
+
+
+/**
+ * fde - fde
+ * @f1: f1
+ * Return: Always 0
+ */
+void fde(va_list f1)
+{
+char c = (char)va_arg(f1, int);
+_putchar(c);
+}
+
+/**
+ * s - s
+ * @f1: f1
+ * Return: Always 0
+ */
+void s(va_list f1)
+{
+int j = 0;
+char *ch;
+ch = malloc(1024);
+ch = va_arg(f1, char*);
+if (!ch)
+{
+ch = "(null)";
+}
+for (j = 0; ch[j]; j++)
+{
+_putchar(ch[j]);
+}
+}
 
 /**
  * _printf - _printf
  * @format: format
  * Return: char
 */
-
 int _printf(const char *format, ...)
 {
 va_list f1;
-int i = 0, j = 0;
-char *stock;
-char *ch;
-stock = malloc(1024);
+int i = 0;
 va_start(f1, format);
-if (!stock)
-return (-1);
+
 while (format && format[i])
 {
 if (format[i] == '%')
@@ -29,29 +56,30 @@ i++;
 switch (format[i])
 {
 case 'c':
-{
-stock[j] = (char)va_arg(f1, int);
-j++;
+fde(f1);
 break;
-}
+case '%':
+_putchar('%');
+break;
+case '\0':
+break;
 case 's':
+s(f1);
+break;
+default:
 {
-ch = va_arg(f1, char*);
-strcpy(&stock[j], ch);
-j += strlen(ch);
+_putchar('%');
+_putchar(format[i]);
 break;
 }
 }
 }
 else
 {
-stock[j] = format[i];
-j++;
+_putchar(format[i]);
 }
 i++;
 }
 va_end(f1);
-write(1, stock, j);
-free(stock);
-return (j);
+return (i);
 }
--- holberton.h
+++ holberton.h
@@ -2,5 +2,6 @@
 #define _PRINTF_
 
 int _printf (const char *format, ...);
+int _putchar(char c);
 
 #endif /* _PRINTF_ */
