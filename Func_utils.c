3f2a32427e53df2e09ab3d6023e39276ee1ebc5b
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-03-19 00:31:39 +0100

    V2

---
+++ Func_utils.c
@@ -0,0 +1,60 @@
+#include "holberton.h"
+/** _strlen - function that that returns the length of a string.
+* @s : pointer to a string.
+* Return: string lenght
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+*_puts -  function that prints a string, //followed by a new line, to stdout.
+**@str : int
+* Return : Void.
+*/
+void _puts(char *str)
+{
+	int i = -1;
+
+	while (str[++i] != '\0')
+		_putchar(str[i]);
+}
+/**
+*_strncat - A function that concatenates two strings.
+*
+*@dest: Pointer to the array.
+*@src: Pointer to the array.
+*@n: number of bytes to copy.
+*Return: Pointer to the resulting string dest.
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int length;
+
+	for (length = 0; dest[length] != '\0'; length++)
+		;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[length + i] = src[i];
+	}
+	dest[length + i] = '\0';
+	return (dest);
+}
---
+++ op_func.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+void write_str(char *src, va_list arg)
+{
+	char *str = va_arg(arg, char *);
+
+	if (str)
+		src = _strncat(src, str, _strlen(str));
+	else
+		src = _strncat(src, "(nil)", 5);
+}
+void write_char(char *src, va_list arg)
+{
+	char  c = va_arg(arg, int);
+
+	if (c)
+		src = _strncat(src, &c, 1);
+}
+void write_per(char *src, va_list arg)
+{
+	char c = '%';
+
+	src = _strncat(src, &c, 1);
+}
