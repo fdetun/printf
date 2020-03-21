13f158307932d0bd4170f2ce279fba88a9149db4
Fakhri Baklouti <cuore.fakhri@gmail.com>
2020-03-21 22:54:40 +0100

    updating with new functions

--- holberton.h
+++ holberton.h
@@ -1,5 +1,6 @@
 #ifndef _PRINTF_
 #define _PRINTF_
+
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
@@ -9,14 +10,19 @@ int _putchar(char c);
 void _puts(char *str);
 char *_strncat(char *dest, char *src, int n);
 int _strlen(char *s);
-void write_per(char *dest,va_list arg);
-void write_char(char *src, va_list arg);
-void write_str(char *src, va_list arg);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+char *write_per(char *dest, va_list arg);
+char *write_char(char *src, va_list arg);
+char *write_str(char *src, va_list arg);
 
 int _printf(const char *format, ...);
 typedef struct Printer
 {
 	char op;
-	void (*p)(char *str,va_list arg);
+	char *(*p)(char *str, va_list arg);
 } Printer;
+
 #endif /* _PRINTF_ */
